f17a3c6c0f46c51c6ba682c5c09bfb7bd1d90c6b
dryfish118 <dryfish118@163.com>
2017-04-05 13:15:29 +0800

    完成取数据的流程

--- chrome-plugin/background.js
+++ chrome-plugin/background.js
@@ -1,83 +1,97 @@
-function failGet(msg) {
-    console.log(msg);
-}
-
-function onList(baseRate, minM, maxM) {
+function onList(maxRate, minM, maxM) {
+    if (maxM - minM > 5000 || minM < 5000) {
+        console.log("poor man");
+        setTimeout(_onList(maxRate, maxM - 1000, maxM), 5 * 1000);
+        return;
+    }
     var fundDetailLink = "https://list.lu.com/list/r030";
+    var strData = "currentPage=1&orderType=R030_INVEST_RATE&orderAsc=false&minMoney=" + minM + "&maxMoney=" + maxM;
     $.ajax({
         url: fundDetailLink,
         dataType: "html",
-        data: {
-            orderType: "R030_INVEST_RATE",
-            orderAsc: "true",
-            minMoney: "" + minM,
-            maxMoney: "" + maxM
-        },
+        data: strData,
         success: function(data) {
             var doc = $(data);
             var productList = doc.find(".product-list");
             if (productList === undefined) {
-                failGet("failed to get product list");
+                console.log("failed to get product list");
+                return;
+            }
+            if (productList.length == 0) {
+                console.log("failed to get product list from %d to %d", minM, maxM);
+                setTimeout(_onList(maxRate, minM - 1000, maxM), 5 * 1000);
                 return;
             }
             productList.each(function() {
                 console.log("product information");
-                var name = $(product).find('.product-name').get(0);
+                var name = $(this).find(".product-name").get(0);
                 if (name === undefined) {
-                    failGet("failed to get product name");
+                    console.log("failed to get product name");
                     return false;
                 }
-                console.log("  product name:\t%s", a.text());
-                console.log("  link:\t%s", a.attr('href'));
+                var a = $(name).find("a");
+                console.log("  product name:\t%s", $(a).text());
+                console.log("  link:\t%s", $(a).attr("href"));
 
-                var rate = $(this).find('.interest-rate .num-style').get(0);
+                var rate = $(this).find(".interest-rate .num-style").get(0);
                 if (rate === undefined) {
-                    failGet("failed to get product rate");
+                    console.log("failed to get product rate");
                     return false;
                 }
-                console.log("  rate:\t%f%%", parseFloat(rate.text()));
+                var dRate = parseFloat($(rate).text());
+                console.log("  rate:\t%f%%", dRate);
+                if (maxRate - dRate > 0.02) {
+                    console.log("low rate");
+                    setTimeout(_onList(maxRate, minM, maxM), 5 * 1000);
+                    return false;
+                }
 
-                var amount = $(this).find('.product-amount .num-style').get(0);
+                var amount = $(this).find(".product-amount .num-style").get(0);
                 if (amount === undefined) {
-                    failGet("failed to get product amount");
+                    console.log("failed to get product amount");
                     return false;
                 }
-                console.log("  amount:\t%f%%", parseFloat(amount.text().replace(',', '')));
+                var dAmount = parseFloat($(amount).text().replace(",", ""));
+                console.log("  amount:\t%f", dAmount);
             });
         },
         error: function() {
-            failGet("failed to get list in onList");
+            console.log("failed to get list in onList");
         }
     });
 }
 
+function _onList(maxRate, minM, maxM) {
+    return function() {
+        onList(maxRate, minM, maxM);
+    };
+}
+
 function onMaxRate(availableMoney) {
     var fundDetailLink = "https://list.lu.com/list/r030";
+    var strData = "currentPage=1&orderType=R030_INVEST_RATE&orderAsc=false";
     $.ajax({
         url: fundDetailLink,
         dataType: "html",
-        data: {
-            orderType: "R030_INVEST_RATE",
-            orderAsc: "true"
-        },
+        data: strData,
         success: function(data) {
             var doc = $(data);
             var product = doc.find(".product-list").get(0);
             if (product === undefined) {
-                failGet("failed to get product in getBaseRate");
+                console.log("failed to get product in onMaxRate");
                 return;
             }
-            var rate = $(product).find('.interest-rate .num-style').get(0);
+            var rate = $(product).find(".interest-rate .num-style").get(0);
             if (rate === undefined) {
-                failGet("failed to get rate in getBaseRate");
+                console.log("failed to get rate in onMaxRate");
                 return;
             }
             var dRate = parseFloat($(rate).text());
-            console("max rate:\t%f", dRate);
+            console.log("max rate:\t%f", dRate);
             onList(dRate, availableMoney - 1000, availableMoney);
         },
         error: function() {
-            failGet("failed to get list in getBaseRate");
+            console.log("failed to get list in onMaxRate");
         }
     });
 }
@@ -86,18 +100,18 @@ function onFundDetail(data) {
     var doc = $(data);
     var amount = doc.find(".available-balance-wrap > .balance-amount").get(0);
     if (amount === undefined) {
-        failGet("failed to get available amount");
+        console.log("failed to get available amount");
         return;
     }
-    var availableMoney = parseFloat($(amount).text().replace(',', ''));
+    var availableMoney = parseFloat($(amount).text().replace(",", ""));
     console.log("available money:\t%f", availableMoney);
 
-    onMaxRate(availableMoney);
+    onMaxRate(parseInt(availableMoney));
 }
 
 function onUserInfo(data) {
     if (data.userName === undefined) {
-        failGet("failed to get user information.");
+        console.log("failed to get user information.");
         return;
     }
     console.log("user id:\t%d", data.uid);
@@ -111,7 +125,7 @@ function onUserInfo(data) {
             onFundDetail(data);
         },
         error: function() {
-            failGet("failed to get fund detail");
+            console.log("failed to get fund detail");
         }
     });
 }
@@ -126,7 +140,7 @@ chrome.browserAction.onClicked.addListener(function() {
             onUserInfo(data);
         },
         error: function() {
-            failGet("failed to get user information.");
+            console.log("failed to get user information.");
         }
     });
 });
