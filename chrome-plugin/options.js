1e9b84439a375edd33da129916309a1a2e2161a5
dryfish118 <dryfish118@163.com>
2017-04-05 21:43:51 +0800

    登录1

--- chrome-plugin/options.js
+++ chrome-plugin/options.js
@@ -4,38 +4,38 @@ function loadConfig() {
     console.log("load config");
     var storage = chrome.storage.local;
     storage.get("username", function(items) {
-        if (items.username) {
-            $("#username").val(items.username);
-        } else {
+        if (items.username === undefined) {
             $("#username").val("");
+        } else {
+            $("#username").val(items.username);
         }
     });
     storage.get("userpass", function(items) {
-        if (items.userpass) {
-            $("#userpass").val(items.userpass);
-        } else {
+        if (items.userpass === undefined) {
             $("#userpass").val("");
+        } else {
+            $("#userpass").val(items.userpass);
         }
     });
     storage.get("minmoney", function(items) {
-        if (items.minmoney) {
-            $("#minmoney").val(items.minmoney);
-        } else {
+        if (items.minmoney === undefined) {
             $("#minmoney").val("5000");
+        } else {
+            $("#minmoney").val(items.minmoney);
         }
     });
     storage.get("stepmoney", function(items) {
-        if (items.stepmoney) {
-            $("#stepmoney").val(items.stepmoney);
-        } else {
+        if (items.stepmoney === undefined) {
             $("#stepmoney").val("500");
+        } else {
+            $("#stepmoney").val(items.stepmoney);
         }
     });
     storage.get("minrate", function(items) {
-        if (items.minrate) {
-            $("#minrate").val(items.minrate);
-        } else {
+        if (items.minrate === undefined) {
             $("#minrate").val("0.02");
+        } else {
+            $("#minrate").val(items.minrate);
         }
     });
 }
--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -9,15 +9,20 @@ function onAquireInNew() {
 
 function onAquireInCurrent(id) {
     console.log("open 'lu.com' in the tab %d", id);
-    strUrl = "https://www.lu.com";
+    var strUrl = "https://www.lu.com";
     chrome.tabs.update({ openerTabId: id, url: strUrl },
         function(tab) {
             onAquireInfo();
         });
 }
 
-function onLogin() {
-
+function onLogin(id) {
+    console.log("logging...");
+    var strUrl = "https://user.lu.com/user/login";
+    chrome.tabs.update({ openerTabId: id, url: strUrl },
+        function(tab) {
+            //
+        });
 }
 
 function onAquireInfo() {
@@ -26,29 +31,34 @@ function onAquireInfo() {
             if (tabs === undefined) {
                 onAquireInNew();
             } else {
+                console.log("current url is '%s'", tabs[0].url);
                 var reg = new RegExp("https://.*\.lu\.com/.*", "");
                 if (!reg.test(tabs[0].url)) {
                     onAquireInCurrent(tabs[0].id);
                     return;
                 }
 
+                console.log("aquire user information");
                 //{"unreadMsgCount":0,"cardBindStatus":"1","uid":28160626,"nameAuthentication":"1",
                 //"sex":"M","name":"閭撴稕","mobileNo":"18511478118","userName":"dengtao118",
                 //"isNewUser":false,"isInvestPrepared":true}
                 var strUrl = "https://user.lu.com/user/service/user/current-user-info-for-homepage";
                 $.ajax({
-                    url: l,
+                    url: strUrl,
                     dataType: "json",
                     success: function(data) {
-                        var h = "<div id='name'>名字：" + data.name + "</div>" +
-                            "<div id='userName'>账号：" + data.userName + "</div>" +
-                            "<div id='uid'>ID：" + data.uid + "</div>" +
-                            "<div id='mobileNo'>电话：" + data.mobileNo + "</div>";
-                        $("#user-name").html(h);
+                        if (data.uid === undefined) {
+                            onLogin(tabs[0].id);
+                        } else {
+                            var h = "<div id='name'>名字：" + data.name + "</div>" +
+                                "<div id='userName'>账号：" + data.userName + "</div>" +
+                                "<div id='uid'>ID：" + data.uid + "</div>" +
+                                "<div id='mobileNo'>电话：" + data.mobileNo + "</div>";
+                            $("#user-name").html(h);
+                        }
                     },
                     error: function() {
-                        console.log("failed to get username");
-                        onLogin();
+                        console.log("failed to get user information");
                     }
                 });
             }
